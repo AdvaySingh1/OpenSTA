ad5731e78eaf12e3bce31c1fa063c7dba56792a1
Advay Singh <Advay@umich.edu>
2025-11-20 16:52:35 -0500

    Simulated module/sub_module* issue

--- tmp/test/sample2.tcl
+++ tmp/test/sample2.tcl
@@ -12,10 +12,22 @@ set top_cells [get_cells -hier *]
 puts "Cell count: [llength $top_cells]"
 puts "First 5 cells: [lrange $top_cells 0 4]"
 
+puts "---------------------------------------"
 
 foreach c $top_cells {
   puts "full_name:      [get_property $c full_name]"
 }
+puts "---------------------------------------"
+puts "PRINTING u_blk* CELLS"
+
+set blk1_cells [get_cells -hier u_blk*]
+
+
+foreach c $blk1_cells {
+  puts "full_name:      [get_property $c full_name]"
+}
+puts "---------------------------------------"
+puts "PRINTING u_blk1/blk* CELLS"
 
 set blk1_sub_cells [get_cells -hier u_blk1/blk*]
 
