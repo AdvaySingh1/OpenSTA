5a4aea3352941c7424e16cbed0de415363fc998b
Advay Singh <Advay@umich.edu>
2025-12-04 10:20:12 -0500

    passing simple regression test and comment fix

--- test/get_cell_hierarchy.tcl
+++ test/get_cell_hierarchy.tcl
@@ -1,5 +1,5 @@
-# hierarchial pattern matching with u_blk1/blk*
-read_liberty ../examples/nangate45_slow.lib
+# hierarchical pattern matching with u_blk1/blk*
+read_liberty ../examples/nangate45_slow.lib.gz
 read_verilog get_cell_hierarchy.v
 link_design dut
 report_object_full_names [get_cells -hier u_blk1/blk*]
